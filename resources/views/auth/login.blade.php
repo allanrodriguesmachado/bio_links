a3b2e0a40e8a4b55f5424c9a8e27444d6995f2cf
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2025-02-01 01:46:36 -0300

    Creating Form Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,15 @@
 <div>
-    Login!!
+    <form action="/login" method="post">
+        @csrf
+
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Username" value="{{old('username')}}">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password" value="{{old('password')}}">
+
+        <div>
+            <button type="submit">Logar</button>
+        </div>
+    </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/login', [\App\Http\Controllers\Auth\LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login']);
